4633b6ff1e205349ff9ec5b1822fb453567f4059
D214238 <70898001+D214238@users.noreply.github.com>
2021-03-09 17:54:40 +0100

    Closed #2 H2

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,16 +2,70 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
-$capaciteit = $_POST['capaciteit']; 
-$melder = $_POST['melder'];
+if(empty($attractie))
+{
+    $errors[] = "Vul de attractie-naam in.";
+}
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+$type = $_POST['type'];
+if(empty($type))
+{
+    $errors[] = "Vul de type in.";
+}
+
+$capaciteit = $_POST['capaciteit'];
+if(!is_numeric($capaciteit))
+{
+    $errors[] = "Vul voor capaciteit een geldig getal in";
+}
+
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = true;
+}
+else
+{
+    $prioriteit = false;
+}
+
+$melder = $_POST['melder'];
+if(empty($melder))
+{
+    $errors[] = "Vul uw naam in.";
+}
+
+$overige_info = $_POST['overige_info'];
+if(empty($overige_info))
+{
+    $errors[] = "Vul een informatieve beschrijving in over het probleem.";
+}
+
+if(isset($errors))
+{
+    var_dump($errors);
+    die();
+}
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
 
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info) VALUES (:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
+
 //3. Prepare
 
+$statement = $conn->prepare($query);
+
 //4. Execute
+
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ":capaciteit" => $capaciteit,
+    ":prioriteit" => $prioriteit,
+    ":melder" => $melder,
+    ":overige_info" => $overige_info
+]);
+
+header("Location:../meldingen/index.php?msg=Meldingopgeslagen");
--- meldingen/create.php
+++ meldingen/create.php
@@ -21,15 +21,33 @@
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select name="type" id="type">
+                    <option value="">-Kies een type-</option>
+                    <option value="achtbaan">Achtbaan</option>
+                    <option value="draaiend">Draaiend</option>
+                    <option value="kinder">Kinder</option>
+                    <option value="horeca">Horeca</option>
+                    <option value="show">Show</option>
+                    <option value="water">Water</option>
+                    <option value="overig">Overig</option>
+                </select>
             </div>
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prioriteit">Prioriteit:</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <label for="prioriteit">Melding met prioriteit</label>
+            </div>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+            <div class="form-group">
+            <label for="overige_info">Overige info:</label>
+                <textarea name="overige_info" id="overige_info" class="form-input" rows="4"></textarea>
             </div>
             
             <input type="submit" value="Verstuur melding">
